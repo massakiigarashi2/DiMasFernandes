eb093a35553d35afe0a24649099759378493015b
Prof. Massaki Igarashi <68185745+lpgroup@users.noreply.github.com>
2023-09-06 16:17:54 -0300

    Update Pesquisa_de_Satisfacao.py

--- Pesquisa_de_Satisfacao.py
+++ Pesquisa_de_Satisfacao.py
@@ -45,4 +45,4 @@ link += str(MSG)
 submit = form.form_submit_button('ENVIAR')
 if submit:
     r = http.request('GET', link)
-st.info("Desenvolvido por: Equipe FabLab / Prof. Massaki de O. Igarashi")
+st.info("Desenvolvido por: Equipe FabLab / Prof. Massaki Igarashi")
